39bcba90d3edee65aedade371631d590d0b3a44e
serj <seregaproger@yandex.ru>
2020-07-22 22:58:54 +0300

    refactoring

--- app.rb
+++ app.rb
@@ -1,11 +1,4 @@
 
 require './logger'
 
-Logger.say_som  #  один объект в пямяти
-
 Logger.instance.log '111'
-Logger.instance.log '222'
-Logger.instance.log '333'
-
-logger = Logger.new
-logger.log 'Hii'
--- logger.rb
+++ logger.rb
@@ -15,23 +15,15 @@ class Logger
     @f = File.open 'log.txt', 'a'
   end
 
-  @@x = nil
+  @@x = Logger.new
 
   def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-
     return @@x
-  end
-
-  #class mathod
-  def self.say_som
-    puts 'hi'
   end
 
   #instans method
   def log wat
     @f.puts wat
   end
+  private_class_method :new
 end
